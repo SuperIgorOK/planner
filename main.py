550894d2bf261281c38c29857cb66ab379bd1962
SuperIgorOK <mcdisel@mail.ru>
2024-03-21 17:48:32 +0300

    final

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
+import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
-from routes.user import user_router
+from fastapi.middleware.cors import CORSMiddleware
+
+from database.connection import Settings
 from routes.events import event_router
-import uvicorn
+from routes.users import user_router
 
 app = FastAPI()
 
+settings = Settings()
+
+# register origins
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # Register routes
-app.include_router(user_router,  prefix="/user")
-app.include_router(event_router,  prefix="/event")
+
+app.include_router(user_router, prefix="/user")
+app.include_router(event_router, prefix="/event")
+
+
+@app.on_event("startup")
+async def init_db():
+    await settings.initialize_database()
+
 
 @app.get("/")
 async def home():
     return RedirectResponse(url="/event/")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     uvicorn.run("main:app", port=8000, reload=True)
